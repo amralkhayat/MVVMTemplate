83aebfa6d725b65d9e7e551563a3e3944f08adec
amralkhayat <33759960+amralkhayat@users.noreply.github.com>
2020-12-22 22:44:37 +0200

    Add files via upload
    Add first version of the MVVM Design pattern

---
+++ ___FILEBASENAME___.swift
@@ -0,0 +1,30 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+
+
+import UIKit
+
+
+
+class ___FILEBASENAMEASIDENTIFIER___ViewController:UIViewController {
+    
+ // MARK: - IBOUTLETS
+    
+ //MARK:- properties
+    lazy  var viewModel:___FILEBASENAMEASIDENTIFIER___ViewModel =  {
+        return ___FILEBASENAMEASIDENTIFIER___ViewModel ()
+    }()
+// MARK: - VIEW LIFECYCLE
+       
+   override func viewDidLoad() {
+       super.viewDidLoad()
+   }
+  
+ //MARK:- IBACTIONS
+  
+    
+    
+}
---
+++ ___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+
+import UIKit
+
+//MARK:- INPUTS Protocol
+protocol ___FILEBASENAMEASIDENTIFIER___Input {
+func viewDidLoad ()
+
+}
+//MARK:- OUTPUTS Protocol
+protocol ___FILEBASENAMEASIDENTIFIER___Output {
+
+}
+
+class ___FILEBASENAMEASIDENTIFIER___ : ___FILEBASENAMEASIDENTIFIER___Input & ___FILEBASENAMEASIDENTIFIER___Output{
+    
+    var input :___FILEBASENAMEASIDENTIFIER___Input{
+        return self
+    }
+    
+    var output:___FILEBASENAMEASIDENTIFIER___Output{
+        return self
+    }
+    
+    
+    
+    //MARK:- Properties
+    
+    
+    
+    //MARK:- INPUTS
+    func viewDidLoad() {
+        
+    }
+    //MARK:- OUTPUTS
+    
+    
+}
